4b9e25773027d2a73ea11d43aa79d0049f1f6e29
Siddharth Kishan <91934415+sidkishan@users.noreply.github.com>
2023-03-25 15:57:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// Your Script here.
+function caeserCipher(str){
+	var ans="";
+	for(var ch in str){
+		if(String.charCodeAt(ch) >= 65 && String.charCodeAt(ch) <= 90){
+			var x = String.charCodeAt(ch);
+			x += 13;
+			x %= 65;
+			ans += String.fromCharCode(x);
+		}
+		else{
+			ans += str[ch];
+		}
+	}
+	return ans;
+}
