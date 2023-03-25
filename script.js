0de53848e5e612518293ede0c7191c1fc0e6f18b
Siddharth Kishan <91934415+sidkishan@users.noreply.github.com>
2023-03-25 16:19:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,48 @@
-function caeserCipher(str){
-	var ans="";
-	for(var ch in str){
-		if(String.charCodeAt(ch) >= 65 && String.charCodeAt(ch) <= 90){
-			var x = String.charCodeAt(ch);
-			x += 13;
-			x %= 65;
-			ans += String.fromCharCode(x);
-		}
-		else{
-			ans += str[ch];
-		}
-	}
-	return ans;
-}
+var lookup = {
+    A: "N",
+    B: "O",
+    C: "P",
+    D: "Q",
+    E: "R",
+    F: "S",
+    G: "T",
+    H: "U",
+    I: "V",
+    J: "W",
+    K: "X",
+    L: "Y",
+    M: "Z",
+    N: "A",
+    O: "B",
+    P: "C",
+    Q: "D",
+    R: "E",
+    S: "F",
+    T: "G",
+    U: "H",
+    V: "I",
+    W: "J",
+    X: "K",
+    Y: "L",
+    Z: "M",
+    "?": "?",
+    ",": ",",
+  };
+
+ function rot13(encodedStr) {
+    var words = encodedStr.split(" "); // String to Array
+    let decodedArr = []; // Your Result goes here
+    // Only change code below this line
+    for (let i = 0; i < words.length; i++) {
+      const word = words[i];
+      let decoded_word = "";
+      for (let j = 0; j < word.length; j++) {
+        var char = word.charAt(j);
+        var decoded_char = lookup[char];
+        decoded_word += decoded_char;
+      }
+      decodedArr.push(decoded_word);
+    }
+    return decodedArr.join(" ");
+  }
+  module.exports = rot13;
